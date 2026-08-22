ad3b5f728d6c539544045ba0568b30cd5bb9b107
commanderpost485-coder <commanderpost8397@gmail.com>
2026-08-22 14:48:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -110,7 +110,22 @@ if(['BID_SUBMITTED','CARD_PLAYED'].includes(type)){
     }
   });
 
-  
+  ws.on('close', () => {
+  if (client.playerId)
+    clients.delete(client.playerId);
+
+  const room = rooms.get(client.roomCode);
+
+  if (room && client.seat) {
+    room.seats[client.seat] = null;
+
+    broadcast(room, 'PLAYER_DISCONNECTED', {
+      playerId: client.playerId,
+      seat: client.seat,
+      seats: room.seats
+    });
+  }
+});
 
 server.listen(PORT,'0.0.0.0',()=>{
   console.log(`Spades Got Real listening on port ${PORT}`);
