e6625aabf946600163812d46c889501db1046519
commanderpost485-coder <commanderpost8397@gmail.com>
2026-08-24 00:48:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -149,6 +149,7 @@ if(['BID_SUBMITTED','CARD_PLAYED'].includes(type)){
   const nextBid = { N: "E", E: "W", W: "S", S: null };
     room.bidTurn = nextBid[client.seat];}
   if (Object.keys(room.bids).length === 4) {
+  room.playTurn = "N";
     const deck = createDeck();
 room.hands = {
   N: deck.slice(0, 13),
@@ -168,6 +169,8 @@ room.hands = {
   }
 }
   broadcast(room, "BIDDING_COMPLETE", { bids: room.bids });
+  }
+return broadcast(room,type,
   }
   return broadcast(room,type,{...payload,serverTs:Date.now()});
 } 
